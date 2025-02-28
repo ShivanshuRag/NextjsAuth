40e4d373437e6fc7561c4cdac3e142313ac6c94e
ShivanshuRag <ragsingh773@gmail.com>
2025-02-28 19:07:21 +0530

    Add user authentication and email handling features
    - Implement user login and signup routes
    - Configure MongoDB connection
    - Create user model with validation
    - Set up email sending functionality for verification and password reset
    - Update package dependencies

---
+++ src/models/usermodel.js
@@ -0,0 +1,37 @@
+
+import mongoose from 'mongoose';
+
+ const userSchema = new mongoose.Schema({
+    username:{
+
+    type: String,
+    require : [true, " please provide a username"],
+    unique: true
+
+    } ,
+
+    email:{
+      type: String,
+      require: [true," please enter email id"],
+      unique: true
+
+    },
+    password:{
+        type: String,
+        require: [true," please enter a password"]
+       
+    },
+    isAdmin:{
+        type: Boolean,
+        default:false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+
+ })
+
+ const User = mongoose.models.users || mongoose.model("users" , userSchema);
+
+ export default User
