14bfa2608e555780c3928b3e7f5888cbce2b2d4a
ShivanshuRag <ragsingh773@gmail.com>
2025-03-03 10:36:31 +0530

    Add user profile retrieval API with token-based authentication

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,19 @@
+import {connect} from "@/dbConfig/dbConfig"
+import { NextRequest, NextResponse } from "next/server"
+import { getDataFromToken } from "@/helpers/getDataFromToken"
+import User from "@/models/usermodel"
+
+connect()
+export async function GET(request: NextRequest){
+  try {
+     const userId = getDataFromToken(request);
+    const user = User.findOne({_id: userId}).select("-password");
+
+    return NextResponse.json({
+        message : "User Found",
+        data: user
+    })
+  } catch (error:any) {
+    return NextResponse.json(error.message, {status: 400})
+  }
+}
