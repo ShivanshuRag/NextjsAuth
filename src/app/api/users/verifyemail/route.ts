a1a44d79c20b2f2f7d4e0b9fc91778192344b28d
ShivanshuRag <ragsingh773@gmail.com>
2025-03-03 16:57:38 +0530

    Fix email verification logic and improve token handling in mailer

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -9,16 +9,21 @@ export async function POST(request: NextRequest){
     try {
       const reqBody = await request.json()
        const {token} = reqBody;
-      
-       const user = await User.findOne({verifyToken: token , verifyTokenExpiry: {$gt: new Date()} })
+      console.log(token);
+       const user = await User.findOne({verifyToken: token , verifyTokenExpiry: {$gt: Date.now()} })
         if(!user){
-            return NextResponse.json({error: "Failed Email Verification"})
+            return NextResponse.json({error: "Failed Email Verification"} , {status:400})
         }
-
-       user.isVerfied = true
-       user.verifyToken = undefined
-       user.verifyTokenExpiry = undefined
-
+     
+       user.isVerfied = true;
+       user.verifyToken = undefined;
+       user.verifyTokenExpiry = undefined;
+       await user.save();
+        
+       return NextResponse.json({
+           message: "Email verified successfully",
+           success: true
+       })
     } catch (error:any) {
       return  NextResponse.json({error: error.message + " IN Verify Email"}, {status: 500})  
     }
